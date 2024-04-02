4132880d244603a1ee672a645127eb2cc099bac4
VIVEK PATEL <patelvk2000@gmail.com>
2024-04-02 01:48:00 +0000

    Added blog search

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,74 @@
-import type { Metadata } from 'next'
+import BlogPost from "@/components/BlogPost";
+import Search from "@/components/Search";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: 'All Blogs',
-	description: 'All blogs I have written.',
-	keywords: 'blogs, writing, articles',
-}
+  title: "All Blogs",
+  description: "All blogs I have written.",
+  keywords: "blogs, writing, articles",
+};
 
-const BlogPage = () => {
-	return (
-		<div className='h-screen flex flex-col items-center mt-40'>
-			<h1 className='text-4xl' >Blog Page</h1>
-		</div>
-	)
-}
+const BlogPage = ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
 
-export default BlogPage
+  const blogs = [
+    {
+      title: "First blog",
+      summary: "This is the first blog",
+      slug: "first-blog",
+    },
+    {
+      title: "Second blog",
+      summary: "This is the Second blog",
+      slug: "second-blog",
+    },
+    {
+      title: "Third blog",
+      summary: "This is the Third blog",
+      slug: "third-blog",
+    },
+  ];
+
+  const filteredBlogs = blogs.filter((blog) =>
+    blog.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return (
+    <div className="h-screen flex flex-col items-center mt-40">
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight text-center text-black dark:text-white">
+        {/* if query Display the number of blogs found */}
+        {query ? (
+          <span>
+            {filteredBlogs.length} blog{filteredBlogs.length === 1 ? "" : "s"}{" "}
+            found
+          </span>
+        ) : (
+          "All Blogs"
+        )}
+        <hr />
+      </h3>
+
+      <div className="w-4/5 grid grid-cols-1 gap-4 p-5 sm:p-10">
+        <Search placeholder="Search blog title" />
+        {/* if query display fileterdblog else show all blog */}
+        {filteredBlogs.length > 0 ? (
+          filteredBlogs.map((blog) => <BlogPost key={blog.slug} {...blog} />)
+        ) : (
+          <p className="text-center text-lg text-gray-600 dark:text-gray-400">
+            No blogs found
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ components/Search.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { Search as Icon } from "lucide-react";
+import { useDebouncedCallback } from "use-debounce";
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        className="peer block w-full border border-gray-300 rounded-md p-2 mt-5py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+      <Icon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    </div>
+  );
+}
