30d874136902d1394cdae9e2141fb7d39eaf947f
patelvk2000 <patelvk2000@gmail.com>
2024-04-20 23:58:02 +0000

    Added Copied to clipboard

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@ import { revalidatePath, unstable_noStore as noStore } from 'next/cache';
 import supabase from '@/lib/supabase/private';
 import { z } from 'zod';
 
-//  Responce type
+//  Response type
 type Response = {
   statusCode: number;
   data: any;
@@ -15,15 +15,17 @@ type Response = {
 export async function increment(slug: string) {
   noStore();
 
-  const { error } = await supabase.rpc('increment', { blog_slug: slug });
+  if (process.env.NODE_ENV !== 'development') {
+    const { error } = await supabase.rpc('increment', { blog_slug: slug });
 
-  if (error) {
-    console.error(error);
+    if (error) {
+      console.error(error);
+    }
+
+    revalidatePath('/');
+    revalidatePath('/blog');
+    revalidatePath(`/blog/${slug}`);
   }
-
-  revalidatePath('/');
-  revalidatePath('/blog');
-  revalidatePath(`/blog/${slug}`);
 }
 
 // =============================== Send Message ===============================
