178366ce722867ce07723e4c5e3f854a9d12db0b
patelvk2000 <patelvk2000@gmail.com>
2024-04-20 06:05:09 +0000

    Remove Spring scroll

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -5,9 +5,12 @@ import { motion } from 'framer-motion';
 
 export default function Progress() {
   const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress);
+  // const scaleX = useSpring(scrollYProgress);
 
   return (
-    <motion.div className='fixed top-0 left-0 right-0 h-[8px] origin-[0%] bg-[#f47067] dark:bg-[#80d1a9]' style={{ scaleX }} />
+    <motion.div
+      className='fixed top-0 left-0 right-0 h-[8px] origin-[0%] bg-[#f47067] dark:bg-[#80d1a9]'
+      style={{ scaleX: scrollYProgress }}
+    />
   );
 }
