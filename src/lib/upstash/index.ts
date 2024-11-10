4e7f5ab26071221fcf3ecbdd6c67690c2e0ab53d
VIVEK PATEL <patelvk2000@gmail.com>
2024-11-10 18:38:45 -0500

    added related posts with embeddings

---
+++ src/lib/upstash/index.ts
@@ -0,0 +1,49 @@
+import { Index } from '@upstash/vector';
+
+export const index = new Index({
+  url: process.env.UPSTASH_VECTOR_REST_URL,
+  token: process.env.UPSTASH_VECTOR_REST_TOKEN,
+});
+
+type Metadata = {
+  title: string;
+  type: 'blog' | 'project';
+  blog?: 'string';
+  project?: 'string';
+};
+
+export const getSimilarPosts = async (
+  slug: string,
+  blogPost: string,
+  type: 'blog' | 'project',
+) => {
+  'use cache';
+  let relatedBlogs = await index.query<Metadata>({
+    data: blogPost,
+    topK: type === 'blog' ? 3 : 2,
+    filter: "type = 'blog'",
+    includeMetadata: true,
+  });
+
+  // remove the current blog post from the related blogs
+  relatedBlogs = relatedBlogs.filter(
+    (post) => post.metadata && post.metadata.blog !== slug,
+  );
+
+  let relatedProjects = await index.query<Metadata>({
+    data: blogPost,
+    topK: type === 'project' ? 3 : 2,
+    filter: "type = 'project'",
+    includeMetadata: true,
+  });
+
+  // remove the current blog post from the related projects
+  relatedProjects = relatedProjects.filter(
+    (post) => post.metadata && post.metadata.project !== slug,
+  );
+
+  return {
+    relatedBlogs,
+    relatedProjects,
+  };
+};
